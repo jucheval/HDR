8adacfb143b43be513d0933ba6f8336513bc3456
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-10-10 17:38:13 +0200

    figure MCRE

--- scripts/call_MCRE.jl
+++ scripts/call_MCRE.jl
@@ -1,4 +1,17 @@
 using MeanFieldGraph
+using Random
+using CairoMakie
+
+function Makie.plot(::Type{MarkovChainModel}, simulation)
+    fig = Figure(size=(600, 300))
+
+    ax = Axis(fig[1, 1], xlabel=L"t", ylabel="neuron")
+    ax.yticks = [1, findfirst(excitatory .== 0), n]
+
+    scatter!(ax, [p.I for p in findall(transpose(simulation.X))], markersize=5, color=:black)
+
+    fig
+end
 
 begin # parameters
     n = 50
@@ -11,16 +24,30 @@ end
 excitatory = MeanFieldGraph.N2excitatory(n, r₊) # encodes the two classes
 
 # simulation
-data = rand(model, excitatory, 100)
+Random.seed!(1)
+simulation = rand(model, excitatory, 100)
+# plot
+fig = Makie.plot(MarkovChainModel, simulation)
 
-begin # plot
-    Random.seed!(1)
-    fig = Figure()
+# save figure
+#save("plots/MCRE-1.png", fig, px_per_unit=2)
 
-    ax = Axis(fig[1, 1], xlabel=L"t", ylabel="neuron")
-    ax.yticks = [1, findfirst(excitatory .== 0), n]
 
-    scatter!(ax, [p.I for p in findall(transpose(data.X))])
-
-    fig
+begin # parameters
+    n = 50
+    μ = 0.3
+    γ = 0.3
+    p = 0.5
+    model = MarkovChainModel(μ, 1 - γ, p)
+    r₊ = 0.4
 end
+excitatory = MeanFieldGraph.N2excitatory(n, r₊) # encodes the two classes
+
+# simulation
+Random.seed!(1)
+simulation = rand(model, excitatory, 100)
+# plot
+fig = Makie.plot(MarkovChainModel, simulation)
+
+# save figure
+#save("plots/MCRE-2.png", fig, px_per_unit=2)

409f140bf11c26943ef56e39d758756f23fa61eb
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-10-10 17:38:30 +0200

    figure coupling KMT

--- scripts/call_KMT.jl
+++ scripts/call_KMT.jl
@@ -7,26 +7,25 @@ begin # time grid
     tmax = len_ts * dt
 end
 
-begin # simulation
-
-end
-
 begin # simulation and plot
-    Random.seed!(1)
-    fig = Figure()
+    Random.seed!(2)
+    fig = Figure(size=(800, 400))
 
     ax = Axis(fig[1, 1], xlabel=L"t")
     kwargs = (; alpha=0.7)
 
     time, X, W = KMT_simulation(len_ts, dt)
     N = counting2point(time, X)
-    lines!(ax, time, X .- time, alpha=0.7, color=:red)
-    lines!(ax, time, W .- time; color=:darkblue, kwargs...)
+    lines!(ax, time, X .- time, alpha=0.7, color=:darkblue)
+    lines!(ax, time, W .- time; color=:orange, kwargs...)
 
     time, X, W = KMT_simulation(len_ts, dt)
     N = counting2point(time, X)
-    lines!(ax, time, X .- time, alpha=0.7, color=:red, linestyle=:dashdot)
-    lines!(ax, time, W .- time; color=:darkblue, linestyle=:dashdot, kwargs...)
+    lines!(ax, time, X .- time, alpha=0.7, color=:darkblue, linestyle=:dashdot)
+    lines!(ax, time, W .- time; color=:orange, linestyle=:dashdot, kwargs...)
 
     fig
 end
+
+# save figure
+#save("plots/coupling_KMT.png", fig, px_per_unit=2)
